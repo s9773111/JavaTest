1f224e9a39167031b2176bcdf329a29757d0babc
isaac <s9773111@gmail.com>
2024-12-12 21:12:25 +0800

    Object

---
+++ src/bomteng/book/Object/Employee.java
@@ -0,0 +1,45 @@
+package bomteng.book.Object;
+
+import java.time.LocalDate;
+
+public class Employee {
+    private String name;
+
+    private double salary;
+    private LocalDate hireDay;
+
+    public Employee(String name, double salary, int year, int month, int day) {
+        this.name = name;
+        this.salary = salary;
+        this.hireDay = LocalDate.of(year, month, day);
+    }
+
+    public void raiseSalary(double byPercent){
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getHireDay() {
+        return hireDay;
+    }
+
+    public void setHireDay(LocalDate hireDay) {
+        this.hireDay = hireDay;
+    }
+}
