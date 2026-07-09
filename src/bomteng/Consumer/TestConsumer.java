410d30240b88291ea590676437fc2d665e28ea79
isaac <s9773111@gmail.com>
2026-07-09 10:14:27 +0800

    update Lambda

--- src/bomteng/Consumer/TestConsumer.java
+++ src/bomteng/Consumer/TestConsumer.java
@@ -1,6 +1,9 @@
 package bomteng.Consumer;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 
@@ -9,11 +12,13 @@ public class TestConsumer {
     public static void main(String[] args) {
         TestConsumer testConsumer = new TestConsumer();
         testConsumer.test_Consumer();
+
+        ex1();
     }
 
     public void test_Consumer() {
         // 1.使用Consumer 介面實現方法
-        // 傳統 匿名類別 : 把「要對每個元素做什麼事」包成一個方法
+        // [傳統] 匿名類別 : 把「要對每個元素做什麼事」包成一個方法
         Consumer<String> consumer = new Consumer<String>(){
             @Override
             public void accept(String s) {
@@ -23,11 +28,9 @@ public class TestConsumer {
         Stream<String> stream = Stream.of("aaa", "bbb", "ddd", "ccc", "fff");
         stream.forEach(consumer); // 呼叫accept
 
-
         System.out.println("********************");
 
-
-        // 2.(簡化)使用lambda表達式, forEach方法需要一個Consumer介面
+        // 2.[簡化] 使用lambda表達式, forEach方法需要一個Consumer介面
         Consumer<String> consumer1 = (s) -> System.out.println(s); //返回是一個consumer介面
         stream = Stream.of("ggg", "kkk", "mmm", "ooo", "qqq");
         stream.forEach(consumer1);
@@ -43,4 +46,25 @@ public class TestConsumer {
         // 更直接方式
         // stream.forEach(System.out::println);
     }
+
+    // 內建函式範例
+    static void ex1() {
+        // 1. Predicate<t> test()
+        Predicate<Integer> isEven = n -> n % 2 == 0;
+        System.out.println("Is 4 even? " + isEven.test(4));
+        System.out.println("Is 7 even? " + isEven.test(7));
+
+        // 2. Function<T, R> apply()
+        Function<String, Integer> stringLength = s -> s.length();
+        System.out.println("Length of 'Hello' : " + stringLength.apply("Hello"));
+
+        // 3. Consumer<T> accept
+        Consumer<String> printOut = s -> System.out.println(s);
+        printOut.accept("This is a test message");
+
+        // 4. Supplier<T> get()
+        Supplier<Double> randomNum = () -> Math.random();
+        System.out.println("Random number : " + randomNum.get());
+    }
+
 }
--- src/bomteng/Lambda/Network/LambdaTest0.java
+++ src/bomteng/Lambda/Network/LambdaTest0.java
@@ -1,15 +1,65 @@
 package bomteng.Lambda.Network;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Lambda 練習
  */
 public class LambdaTest0 {
     public static void main(String[] args) {
+        ex1();
+
+        ex2();
+    }
+
+    static void ex1() {
         Calculator plus = (a, b) -> a + b;
         Calculator multi = (a, b) -> a * b;
 
         System.out.println("lambda 5,3 加法: " + plus.compute(5,3));
         System.out.println("lambda 2,8 乘法: " + multi.compute(2,8));
+    }
+
+    static void ex2() {
+        // 1. Using Runnable interface
+        Runnable task = () -> System.out.println("Executing task");
+        new Thread(task).start();
+        // 舊有寫法1
+        Runnable task1 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Executing task1");
+            }
+        };
+        new Thread(task1).start();
+
+        // 舊有寫法2
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Executing task2");
+            }
+        }).start();
+
+
+        // 2. Using Comparator for sorting
+        List<String> names = Arrays.asList("Isaac", "Quanqiu", "Japan", "cat");
+        Collections.sort(names, (a, b) -> a.length() - b.length());
+        System.out.println("Sorted names: " + names);
+        // 舊有寫法
+        Collections.sort(names, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return a.length() - b.length();
+            }
+        });
+
+        // 3.Using Predicate for filtering
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+        numbers.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
     }
 }
 
